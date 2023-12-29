1d58cf1fd7b76cc734c225a3ba645348d86e9745
realtobi999 <tobiasfilgas@gmail.com>
2023-12-29 22:15:57 +0100

    [fix] update not updating when null

--- app/Http/Controllers/PasswordController.php
+++ app/Http/Controllers/PasswordController.php
@@ -43,21 +43,22 @@ class PasswordController extends Controller
     {
         try {
             $attributes = request()->validate([
-                "website" => ["max:255"],
-                "username" => ["max:255"],
-                "password" => ["nullable", "min:8", "max:255"],
+                "website" => ["nullable", "max:255"],
+                "username" => [ "nullable", "max:255"],
+                "password" => ["nullable", "min:8" ,"max:255"],
             ]);
+            $attributes = $this->WhereNotNull($attributes);
 
             $this->authorizeUserOrFail();
 
             Passwords::find(request("passwordID"))->update($attributes);
+            return redirect("/vault/dashboard")->with("success", "Successfully updated!");
 
-            return redirect("/vault/dashboard")->with("success", "Password updated!");
         } catch (\Illuminate\Validation\ValidationException $e) {
             // Catch validation errors
             return redirect("/vault/dashboard")->withErrors($e->errors())->withInput();
         } catch (\Exception $e) {
-            // Catch other exceptions
+            // Catch other exceptions (authentication errors)
             return redirect("/vault/dashboard")->with("error", $e->getMessage());
         }
     }
@@ -91,5 +92,17 @@ class PasswordController extends Controller
         if ($passwordID && ($passwordID->user_id != auth()->user()->id)) {
             throw new \Exception("Unauthorized action!");
         }
+    }
+
+    public function whereNotNull($array) {
+        // Convert the array to a collection using collect()
+        $collection = collect($array);
+    
+        // Use the filter method to get non-null values
+        $filteredAttributes = $collection->filter(function ($value) {
+            return $value !== null;
+        });
+    
+        return $filteredAttributes->toArray();
     }
 }
