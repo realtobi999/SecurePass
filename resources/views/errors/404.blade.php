cff34c26017ab313599809ef90813b58fec62b1f
realtobi999 <tobiasfilgas@gmail.com>
2023-12-30 22:50:43 +0100

    [built] view for 404 error

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,17 @@
+@extends("layout.home")
+
+@section("content")
+
+<div>
+    <h1 class="font-bold text-center mt-10 mb-6 text-4xl ">SecurePass <span class="text-blue-300 hover:text-blue-600 transition">Vault</span></h1>
+</div>
+<div class="mt-10 mb-5">
+    <div>
+        <div class="text-center">
+            <p class="text-red-500 text-2xl font-semibold p-3">Oh, no! Page not found!</p>
+            <a href="/" class="text-blue-500 hover:text-blue-600 transition">Go home</a>
+        </div>
+    </div>
+</div>
+
+@endSection
