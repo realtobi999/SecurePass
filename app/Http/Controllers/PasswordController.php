0137c931c18a8943a9a7b3fa4e5eba9a76bcc1ef
realtobi999 <tobiasfilgas@gmail.com>
2023-12-31 02:57:29 +0100

    [fix] wrong old value data for store view

--- app/Http/Controllers/PasswordController.php
+++ app/Http/Controllers/PasswordController.php
@@ -15,40 +15,40 @@ class PasswordController extends Controller
         return auth()->user()->passwords;
     }
 
-    /**
-     * Store a new password.
-     *
-     * @return \Illuminate\Http\RedirectResponse
-     */
+
     public function store()
     {
-        $attributes = request()->validate([
-            "website" => ["required", "string" ,"max:255"],
-            "username" => ["required", "string" ,"max:255"],
-            "password" => ["required", "string" ,"min:8" ,"max:255"],
-            "uri" => ["nullable","string" ,"max:255"],
-        ]);
+        try {
+            $attributes = request()->validate([
+                "website" => ["required", "string", "max:255"],
+                "username" => ["required", "string", "max:255"],
+                "password" => ["required", "string", "min:8", "max:255"],
+                "uri" => ["nullable", "string", "max:255"],
+            ]);
 
-        $attributes["user_id"] = auth()->user()->id;
+            $attributes["user_id"] = auth()->user()->id;
 
-        Passwords::create($attributes);
+            Passwords::create($attributes);
 
-        return redirect("/vault/dashboard")->with("success", "Password saved!");
+            return redirect("/vault/dashboard")->with("success", "Password saved!");
+        } catch (\Illuminate\Validation\ValidationException $e) {
+            // Catch validation errors
+            return redirect("/vault/store")->withErrors($e->errors())->withInput();
+        } catch (\Exception $e) {
+            // Catch other exceptions (authentication errors)
+            return redirect("/vault/dashboard")->with("error", $e->getMessage());
+        }
     }
 
-    /**
-     * Update an existing password.
-     *
-     * @return \Illuminate\Http\RedirectResponse
-     */
+
     public function update()
     {
         try {
             $attributes = request()->validate([
                 "website" => ["nullable", "max:255"],
-                "username" => [ "nullable", "max:255"],
-                "password" => ["nullable", "min:8" ,"max:255"],
-                "uri" => ["nullable", "max:255"],
+                "username" => ["nullable", "max:255"],
+                "password" => ["nullable", "min:8", "max:255"],
+                "uri" => ["nullable", "string", "max:255"],
             ]);
             $attributes = $this->WhereNotNull($attributes);
 
@@ -56,7 +56,6 @@ class PasswordController extends Controller
 
             Passwords::find(request("passwordID"))->update($attributes);
             return redirect("/vault/dashboard")->with("success", "Successfully updated!");
-
         } catch (\Illuminate\Validation\ValidationException $e) {
             // Catch validation errors
             return redirect("/vault/dashboard")->withErrors($e->errors())->withInput();
@@ -66,11 +65,7 @@ class PasswordController extends Controller
         }
     }
 
-    /**
-     * Delete a password.
-     *
-     * @return \Illuminate\Http\RedirectResponse
-     */
+
     public function destroy()
     {
         try {
@@ -83,11 +78,7 @@ class PasswordController extends Controller
         }
     }
 
-    /**
-     * Authorize the user for the requested password action.
-     *
-     * @throws \Exception
-     */
+
     public function authorizeUserOrFail()
     {
         $passwordID = Passwords::find(request("passwordID"));
@@ -97,15 +88,16 @@ class PasswordController extends Controller
         }
     }
 
-    public function whereNotNull($array) {
-        // Convert the array to a collection using collect()
+    public function whereNotNull($array)
+    {
+
         $collection = collect($array);
-    
-        // Use the filter method to get non-null values
+
         $filteredAttributes = $collection->filter(function ($value) {
             return $value !== null;
         });
-    
+
         return $filteredAttributes->toArray();
     }
+
 }
